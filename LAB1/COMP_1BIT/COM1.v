cc39058118edd9581db0426972a9ba1545eb71cb
Suhani <suhanism1010@gmail.com>
2025-08-23 00:09:05 +0530

    Update COM1.v

--- LAB1/COMP_1BIT/COM1.v
+++ LAB1/COMP_1BIT/COM1.v
@@ -1,13 +1,13 @@
 module comp1 (
     input A,
     input B,
-    output o1,  // A > B
-    output o2,  // A == B
-    output o3   // A < B
+    output o1,  
+    output o2,  
+    output o3   
 );
 
-assign o1 = A & ~B;     // A > B
-assign o2 = ~(A ^ B);   // A == B
-assign o3 = ~A & B;     // A < B
+assign o1 = A & ~B;     
+assign o2 = ~(A ^ B);   
+assign o3 = ~A & B;     
 
 endmodule
